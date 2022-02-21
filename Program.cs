50b2864d3b8f709018cde9a179b11469fcc82897
marv7000 <marv7000@web.de>
2022-02-21 21:30:51 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,14 +14,13 @@ namespace MRAOcombine
             Bitmap AO = null;
             
             string path = "";
-
             foreach (string arg in args)
             {
                 if (arg.Contains("Metal"))
                 {
                     M = new Bitmap(arg);
                     Console.WriteLine("Metallic:\t" + arg);
-                    path = arg.Replace(".png", "_MRAO.png");
+                    path = arg.Replace(".", "_MRAO.");
                 }
                 else if (arg.Contains("Rough"))
                 {
@@ -36,22 +35,27 @@ namespace MRAOcombine
                 else
                     throw new ArgumentException();
             }
-
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("[WARN] You seem to be using the tool incorrectly. You need to drag files onto the .exe to convert them.");
+                Console.ReadLine();
+                throw new NullReferenceException();
+            }
             Bitmap Out = new Bitmap(M.Width, M.Height);
             for (int x = 0; x < M.Width; x++)
             {
-                for(int y = 0; y < M.Height; y++)
+                for (int y = 0; y < M.Height; y++)
                 {
                     // Use R channels to set RGB channels on the output texture
-                    Out.SetPixel(x, y, Color.FromArgb(255,M.GetPixel(x,y).R, R.GetPixel(x, y).R, AO.GetPixel(x, y).R));
+                    if (AO != null)
+                        Out.SetPixel(x, y, Color.FromArgb(255, M.GetPixel(x, y).R, R.GetPixel(x, y).R, AO.GetPixel(x, y).R));
+                    else
+                        Out.SetPixel(x, y, Color.FromArgb(255, M.GetPixel(x, y).R, R.GetPixel(x, y).R, 255));
                 }
             }
-
-
             // Save to file
             Out.Save(path);
             Console.WriteLine("\nSaved file to " + path);
-
             Console.ReadLine();
         }
     }
